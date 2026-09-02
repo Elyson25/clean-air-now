abcf818f2bb8a1ae162b50d26cefd62bab0a2bd1
Elyson25 <eliud8790@gmail.com>
2026-09-03 02:42:43 +0300

    conseuences of errors

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import postcss from './postcss.config.cjs'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  css: {
-    postcss: postcss,
-  },
 })
